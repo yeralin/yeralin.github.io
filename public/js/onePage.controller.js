b9821125174db822647a4fcf8ad6ca523009380d
Daniyar Yeralin <daniyar.yeralin@gmail.com>
2014-12-03 18:08:26 -0500

    Upload

--- public/js/onePage.controller.js
+++
@@ -1,6 +0,0 @@
-var pageController = angular.module('pageController', ['ngRoute']).config(function($routeProvider){
-	$routeProvider.when('/text-content', {
-        templateUrl: 'text-content.html'
-    });
-    $routeProvider.otherwise({redirectTo:'text-content'});
-});
