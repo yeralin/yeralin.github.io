6f2cf0565ac06b2286b8e1c6bf89c13694a1011c
Daniyar Yeralin <daniyar.yeralin@gmail.com>
2015-08-04 23:59:29 -0400

    New

---
+++ js/index.js
@@ -0,0 +1,87 @@
+$(window).load(function() {
+    NProgress.done();
+});
+
+
+$(document).ready(function() {
+	$(".one_page_main").onepage_scroll();
+    NProgress.start();
+    
+});
+
+var state = 0;
+
+$(document).keydown(function(e){
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '39') {
+		if(state == 0) {
+			$(".one_page_main").moveDown();
+		}
+		else if(state == 1) {
+			$(".line").addClass("active");
+		}
+		else if(state == 2) {
+			$(".one_page_main").moveDown();
+		}
+		else if(state == 3) {
+			$(".blockImg").addClass("active");
+		}
+		else if(state == 4) {
+			$(".below").addClass("active");
+		}
+		else if(state == 5) {
+			$(".one_page_main").moveDown();
+		}
+		else if(state == 6){
+			animateAnswer();
+		}
+		else if(state == 7) {
+			$(".block").removeClass("active");
+			$(".ex").text("How it works?");
+			$(".how").addClass("active");
+		}
+		else if(state == 8)
+			$(".one_page_main").moveDown();
+		else if(state == 9) {
+			$(".videoBlock").addClass("active")
+		}
+		else if(state == 10) {
+			$("#video1")[0].play();
+		}
+		else if(state == 11) {
+			$("#video2")[0].play();
+		}
+		else if(state == 12) {
+			$("#video3")[0].play();
+		}
+		else if(state == 13) {
+			$("#video4")[0].play();
+		}
+		else if(state == 14) {
+			$('.bxslider').bxSlider({
+				auto: true,
+				pause: 8000
+			});
+			$(".one_page_main").moveDown();
+		}
+		else if(state == 15) {
+
+			$(".one_page_main").moveDown();
+		}
+		state++;
+    }
+});
+
+
+var animateAnswer = function(callback) {
+	$(".exInput").typed({
+		strings: ["What's temperature outside?"],
+		typeSpeed: 0,
+		callback: function() {
+			$(".arrow").addClass("active");
+			setTimeout(function() {
+				$(".answer").addClass("active");
+			}, 2000);
+		}
+	});
+};
